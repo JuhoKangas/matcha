0b30d32df2cc0dc0041385260c2bb3d10a7b19ca
Juho Kangas <jkangas@student.hive.fi>
2022-11-30 16:21:05 +0200

    add settings.json for tailwind intellisense to work properly

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,9 @@
 const Title = ({ title }) => {
-  return <h1 className="text-3xl font-bold underline">{title}</h1>
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold underline text-red-600">{title}</h1>
+    </div>
+  )
 }
 
 export default Title
