4fda3318f28504f1801b504557fa3b12e80eb978
Juho Kangas <jkangas@student.hive.fi>
2022-11-30 15:20:43 +0200

    initialize tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Title from './components/Title'
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title title="Hello World" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Title.js
@@ -0,0 +1,5 @@
+const Title = ({ title }) => {
+  return <h1 className="text-3xl font-bold underline">{title}</h1>
+}
+
+export default Title
