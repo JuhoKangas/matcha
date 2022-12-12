04fe6074113743660d272398ef054aced3d0c32d
Juho Kangas <jkangas@student.hive.fi>
2022-12-12 18:15:33 +0200

    create hook for input field

--- client/src/routes/Register.js
+++ client/src/routes/Register.js
@@ -1,7 +1,18 @@
+import { useField } from '../hooks'
+
 const Register = (props) => {
+  const username = useField('text')
+
   return (
     <div>
       <h1>Register</h1>
+      <div>
+        username
+        <input
+          className="border-2 rounded border-neutral-800 ml-4 outline-none focus:border-slate-500"
+          {...username}
+        />
+      </div>
     </div>
   )
 }
