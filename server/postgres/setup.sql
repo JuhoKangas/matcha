515c8838cbd98cd307055b582d08c06ca4c53e36
Alba Castano <acastano@student.hive.fi>
2022-12-16 13:06:27 +0200

    Added columns (email, password, age, ...) to users table

--- server/postgres/setup.sql
+++ server/postgres/setup.sql
@@ -3,7 +3,19 @@ CREATE TABLE IF NOT EXISTS users (
   username VARCHAR(1000) NOT NULL,
   firstname VARCHAR(1000) NOT NULL,
   lastname VARCHAR(1000) NOT NULL,
+	email VARCHAR(1000), 
+	password VARCHAR(1000) NOT NULL,
   age INT NOT NULL,
+  gender_identity VARCHAR(1000) NOT NULL,
+	gender_interest VARCHAR(1000) DEFAULT 'Everyone',
+  tags INT NOT NULL,
+	bio VARCHAR(1000) DEFAULT 'none',
+	city VARCHAR(1000),
+	country VARCHAR(1000),
+	actual_location VARCHAR(1000), /* complete this */
+	active SMALLINT DEFAULT 0,
+	token varchar(255) NOT NULL DEFAULT 0,
+	fame INT NOT NULL DEFAULT 0,
+	online SMALLINT DEFAULT 0,
+	last_seen TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
-
-INSERT INTO users(username, firstname, lastname, age) VALUES ('juhok', 'juho', 'kangas', 31);
