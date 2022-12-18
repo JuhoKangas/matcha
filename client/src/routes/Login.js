82f642872a144af14e0202dfbfc348edd932cf8f
Katarina-Slotova <katarina.slotova@gmail.com>
2022-12-18 17:44:19 +0200

    Change Login page styling, add new font.

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -33,54 +33,80 @@ const Login = ({ setToken }) => {
   }
 
   return (
-    <div className="">
-      <h1 className="">Log in to your account</h1>
+	  
+	<div className="flex flex-col">
 
-      <h3 className="">Login Form</h3>
-      <form onSubmit={login}>
-        <div className="">
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="mt-1
-			block
-			w-full
-			rounded-md
-			border-gray-300
-			shadow-sm
-			focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            required
-          />
-          <label className="" htmlFor="email">
-            Email
-          </label>
-        </div>
+	<div>
+		<h1 className="text-center font-montserrat font-bold leading-tight text-4xl mt-20 mb-10">Log in to your account</h1>
+	</div>
 
-        <div className="">
-          <input
-            type="password"
-            name="password"
-            id="password"
-            className="mt-1
-			block
-			w-full
-			rounded-md
-			border-gray-300
-			shadow-sm
-			focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            required
-          />
-          <label className="" htmlFor="password">
-            Password
-          </label>
-        </div>
+	<div className="flex justify-center items-center">
+		
+    	<form onSubmit={login} className="bg-white shadow-sm rounded px-10 pt-6 pb-8 mb-4">
+			<div className="mb-4">
+				<label className="block font-montserrat mb-2" htmlFor="email">
+					Email
+				</label>
+				<input
+					type="email"
+					name="email"
+					id="email"
+					placeholder="Email"
+					className="mt-1
+					font-montserrat
+					block
+					w-full
+					rounded-md
+					border-gray-300
+					shadow-sm
+					text-gray-700
+					focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-20"
+					required
+				/>
+			</div>
 
-        <div className="">
-          <input className="btn-purple-moon " type="submit" value="Login" />
-        </div>
-      </form>
+			<div className="mb-6">
+				<label className="block font-montserrat" htmlFor="password">
+					Password
+				</label>
+				<input
+					type="password"
+					name="password"
+					id="password"
+					placeholder="******************"
+					className="mt-1
+					font-montserrat
+					block
+					w-full
+					rounded-md
+					border-gray-300
+					shadow-sm
+					text-gray-700
+					focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-20"
+					required
+				/>
+			</div>
+
+			<div className="flex items-center justify-center mb-5">
+				<input className="bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-l text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-montserrat" type="submit" value="Login" />
+			</div>
+			<hr/>
+			<div className="flex items-center justify-center mt-5 mb-5">
+				<a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+					Forgot password?
+				</a>
+			</div>
+			<hr/>
+			<div className="flex items-center justify-center mt-5">
+				<a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+					Already have an account? Sign in!
+				</a>
+			</div>
+      	</form>
     </div>
+	
+	</div>
+	
   )
 }
 
