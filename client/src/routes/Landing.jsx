dbee063a74be96ed24bfe8ead370f098c6ea29ca
Katarina Slotova <kslotova@c2r2p11.hive.fi>
2022-12-19 11:31:55 +0200

    Modify styling of landing page.

--- client/src/routes/Landing.jsx
+++ client/src/routes/Landing.jsx
@@ -1,57 +1,52 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import '../styles.css'
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import "../styles.css"
 // import { useSelector } from 'react-redux'
 
 const Landing = () => {
   const navigate = useNavigate()
 
   const navigateLogin = () => {
-    navigate('/login')
+    navigate("/login")
   }
 
   const navigateSignup = () => {
-    navigate('/register')
+    navigate("/register")
   }
 
   return (
-    <div
-      className="page-holder align-items-center py-4 bg-gray-100 vh-100 landing-bg"
-      style={{ background: 'black' }}
-    >
-      <div className="container">
-        <h1 className="text-light landing-heading">
-          Find your perfect match with us!
-        </h1>
-        <div className="row align-items-center">
-          <div className="col-lg-6 col-xl-5 ms-xl-auto px-lg-4 text-center text-primary">
-            <div style={{ padding: '10px' }}>
-              <button
-                className="col btn btn-purple-moon btn-lg"
-                onClick={navigateLogin}
-              >
-                Log in
-              </button>
-            </div>
-            <div style={{ padding: '10px' }}>
-              <button
-                className="col btn btn-pink-moon btn-lg"
-                onClick={navigateSignup}
-              >
-                Sign up
-              </button>
-            </div>
+    <div className="flex flex-col bg-black">
+      <h1 className="text-center font-montserrat font-style: italic leading-tight text-4xl mt-20 mb-10 text-white">
+        Find your perfect match with us!
+      </h1>
+      <div className="flex justify-center items-between">
+        <div className="">
+          <div style={{ padding: "10px" }}>
+            <button
+              className="bg-gradient-to-r from-sky-500 to-indigo-500 hover:bg-gradient-to-l text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-montserrat"
+              onClick={navigateLogin}
+            >
+              Log in
+            </button>
           </div>
-          <div className="col-lg-6 col-xl-5 ms-xl-auto px-lg-4 text-center text-primary">
-            <img
-              className="img-landing img-fluid mb-4"
-              width="350"
-              height="350"
-              src="matcha-logo-dark.png"
-              alt="matcha-logo"
-              style={{ transform: 'rotate(10deg)' }}
-            ></img>
+          <div style={{ padding: "10px" }}>
+            <button
+              className="bg-gradient-to-l to-purple-400 from-purple-600 hover:bg-gradient-to-r text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline font-montserrat"
+              onClick={navigateSignup}
+            >
+              Sign up
+            </button>
           </div>
+        </div>
+        <div className="">
+          <img
+            className=""
+            width="350"
+            height="350"
+            src="matcha-logo-dark.png"
+            alt="matcha-logo"
+            style={{ transform: "rotate(10deg)" }}
+          ></img>
         </div>
       </div>
     </div>
