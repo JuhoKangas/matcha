1749864ec895e9d9d22c7e5173fe4553d4bf2451
Alba Castano <acastano@student.hive.fi>
2023-01-16 22:19:11 +0200

    clean for merge, left TODO's and current

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,42 +1,7 @@
-import usersService from '../services/users'
-import { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
 import UsersList from '../components/UsersList'
 import Filters from '../components/Filters'
 
 const Home = (props) => {
-  // const dispatch = useDispatch()
-  // const user = useSelector(({ user }) => user)
-
-  // const filters = useSelector(({ filters }) => filters)
-
-  // useEffect(() => {
-  //   dispatch(initializeFilters())
-  // }, [dispatch])
-
-  // if (filters.length > 0) {
-  //   const filteredList = sortedList.filter((a) =>
-  //     a.content.toLowerCase().includes(filters.toLowerCase())
-  //   )
-  //   sortedList = filteredList
-  // }
-  // TODO: sort by location
-  // sortedList.sort((a, b) => b.distance - a.distance)
-
-  // const [lastDirection, setLastDirection] = useState()
-
-  // const swiped = (direction, nameToDelete) => {
-  //   console.log('removing: ' + nameToDelete)
-  //   setLastDirection(direction)
-  // }
-
-  // const outOfFrame = (name) => {
-  //   console.log(name + ' left the screen!')
-  // }
-
-  // console.log('in home sortedList is: ', sortedList)
-  // console.log('in home filters is: ', filters)
-
   return (
     <div>
       <div
