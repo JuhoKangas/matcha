bf6a549f1454297744fe71fc9daa2fc35701aa63
Juho Kangas <jkangas@student.hive.fi>
2023-01-31 16:25:23 +0200

    install js-cookies and loader-spinner, conditional rendering for loading screen, still have to fix css definitely

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
@@ -12,16 +12,39 @@ import Photos from './routes/Photos'
 import Blocked from './routes/Blocked'
 import Setup from './routes/Setup'
 import Footer from './components/Footer'
+import { Hearts } from 'react-loader-spinner'
 
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Home from './routes/Home'
 import Landing from './routes/Landing'
 import { Toaster } from 'react-hot-toast'
 import authService from './services/auth'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import { setUser } from './reducers/userReducer'
 
 const App = () => {
   const user = useSelector(({ user }) => user)
-  authService.checkToken().then((res) => console.log('res is ', res))
+  const dispatch = useDispatch()
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    if (Cookies.get('authorization')) {
+      setIsLoading(true)
+      authService
+        .checkToken()
+        .then((res) => {
+          axios.get(`http://localhost:3001/users/${res.id}`).then((res) => {
+            dispatch(setUser(res.data.data.rows[0]))
+            setTimeout(() => {
+              setIsLoading(false)
+            }, 3000)
+          })
+        })
+        .catch((err) => console.log(err))
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div className="flex flex-col justify-between min-h-screen bg-almost-black">
@@ -29,23 +52,36 @@ const App = () => {
         {user.bio && <Navbar user={user} />}
         <Toaster position="top-center" reverseOrder={false} />
         <div className="grow">
-          <Routes>
-            <Route path="/" element={<Landing />}></Route>
-            {user.bio && <Route path="/home" element={<Home />}></Route>}
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/register" element={<Register />} />
-            {user.bio && <Route path="/matches" element={<Matches />} />}
-            {user.bio && <Route path="/browse" element={<Browse />} />}
-            {user.bio && (
-              <Route path="/profile" element={<Profile user={user} />} />
-            )}
-            {user.bio && (
-              <Route path="/settings" element={<Settings user={user} />} />
-            )}
-            {user.bio && <Route path="/photos" element={<Photos />} />}
-            {user.bio && <Route path="/blocked" element={<Blocked />} />}
-            {<Route path="/setup" element={<Setup user={user} />} />}
-          </Routes>
+          {isLoading ? (
+            <Hearts
+              height="80"
+              width="80"
+              color="#007991"
+              ariaLabel="hearts-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+              className="mx-auto my-auto"
+            />
+          ) : (
+            <Routes>
+              <Route path="/" element={<Landing />} />
+              {user.bio && <Route path="/home" element={<Home />} />}
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              {user.bio && <Route path="/matches" element={<Matches />} />}
+              {user.bio && <Route path="/browse" element={<Browse />} />}
+              {user.bio && (
+                <Route path="/profile" element={<Profile user={user} />} />
+              )}
+              {user.bio && (
+                <Route path="/settings" element={<Settings user={user} />} />
+              )}
+              {user.bio && <Route path="/photos" element={<Photos />} />}
+              {user.bio && <Route path="/blocked" element={<Blocked />} />}
+              <Route path="/setup" element={<Setup user={user} />} />
+            </Routes>
+          )}
         </div>
       </Router>
       <Footer />
