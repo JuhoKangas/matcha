81723c1c98e47f3b93a66053b04dc7850b47cc38
Katarina Slotova <kslotova@c2r2p10.hive.fi>
2023-02-22 13:24:35 +0200

    Notifications page done.

--- server/controllers/notifications.js
+++ server/controllers/notifications.js
@@ -2,9 +2,11 @@ const notificationsRouter = require('express').Router()
 const db = require('../db/index')
 
 notificationsRouter.get('/', async (req, res) => {
+	let seen = 't'
 	const data = req.query
 	try {
-		const result = await db.query('SELECT * FROM notifications WHERE recipient = $1', [data.loggedInUser])
+		const result = await db.query('SELECT * FROM notifications WHERE recipient = $1 ORDER BY id DESC', [data.loggedInUser])
+		const update = await db.query('UPDATE notifications SET seen = $1 WHERE recipient = $2', [seen, data.loggedInUser])
 		const allNotifications = result.rows
 		res.status(200).json({
 			status: 'success',
