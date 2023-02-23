a6bf1094944fc43a8c85187aac57fa9f898d8a9c
Alba Castano <acastano@student.hive.fi>
2023-02-23 19:38:32 +0200

    Moved displaying criteria of being liked/unliked to UsersList. Liking users in UsersProfile page working.

--- client/src/components/UsersList.js
+++ client/src/components/UsersList.js
@@ -2,10 +2,13 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import Card from './Card'
 
-const UsersList = ({socket}) => {
+const UsersList = ({ socket }) => {
   const loggedInUser = useSelector(({ user }) => user)
   const filteredUsers = useSelector(({ filteredUsers }) => filteredUsers)
   const sortingFilter = useSelector(({ sortingFilters }) => sortingFilters)
+
+  const likes = useSelector(({ likes }) => likes)
+  const unlikes = useSelector(({ unlikes }) => unlikes)
 
   const commonTags = (user1, user2) => {
     const common = user1.tags.filter((tag) => user2.tags.includes(tag))
@@ -30,11 +33,31 @@ const UsersList = ({socket}) => {
     sortedUsers.sort((a, b) => a.distance - b.distance)
   }
 
+  function passesLikes(u) {
+    if (
+      likes.find(
+        (like) => like.user1 === loggedInUser.id && like.user2 === u.id
+      ) ||
+      unlikes.find(
+        (unlike) =>
+          (unlike.user1 === loggedInUser.id && unlike.user2 === u.id) ||
+          (unlike.user2 === loggedInUser.id && unlike.user1 === u.id)
+      )
+    ) {
+      return false
+    }
+    return true
+  }
+
   return (
     <div className="flex flex-col gap-8">
-      {sortedUsers.map((u) => (
-        <Card key={u.id} user={u} socket={socket} />
-      ))}
+      {sortedUsers.map((u) =>
+        passesLikes(u) === true ? (
+          <Card key={u.id} user={u} socket={socket} />
+        ) : (
+          ''
+        )
+      )}
     </div>
   )
 }
--- client/src/routes/UserProfile.js
+++ client/src/routes/UserProfile.js
@@ -1,84 +1,108 @@
-import React, { useEffect } from 'react'
-// import { createChat } from '../reducers/chatReducer'
+import React, { useEffect, useRef } from 'react'
 import { HeartIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import moment from 'moment'
-//import usersService from '../services/users'
+import { likeUser } from '../reducers/likesReducer'
 
 const UserProfile = ({ socket, selectedUser }) => {
-  console.log('In other user profile page, user info: ', selectedUser)
-  // const dispatch = useDispatch()
-  const loggedUser = useSelector(({ user }) => user)
+  const loggedInUser = useSelector(({ user }) => user)
+  const likes = useSelector(({ likes }) => likes)
+  // const unlikes = useSelector(({ unlikes }) => unlikes)
 
-	const showOnlineStatus = () => {
-		const status = selectedUser.online === 1 ? "ONLINE" : moment(selectedUser.lastSeen).format('MMMM Do, h:mm:ss a')
-		return status
-	}
+  const hasLiked = likes.find(
+    (like) => like.user1 === loggedInUser.id && like.user2 === selectedUser.id
+  )
 
-  // const setupChat = () => {
-  //   const newChat = {
-  //     loggedUserId: loggedUser.id,
-  //     loggedUserImg: loggedUser.profilePicture,
-  //     loggedUserUsername: loggedUser.username,
-  //     recipientId: selectedUser.id,
-  //     recipientImg: selectedUser.profile_picture,
-  //     recipientUsername: selectedUser.username,
-  //   }
-  //   dispatch(createChat(newChat))
-  // }
+  // const hasUnliked = unlikes.find(
+  //   (unlike) =>
+  //     unlike.user1 === loggedInUser.id && unlike.user2 === selectedUser.id
+  // )
+
+  const showOnlineStatus = () => {
+    const status =
+      selectedUser.online === 1
+        ? 'ONLINE'
+        : moment(selectedUser.lastSeen).format('MMMM Do, h:mm:ss a')
+    return status
+  }
 
   useEffect(() => {
-    console.log('ONE IN USERPROFILE')
     socket.emit('notification', {
-      user1: loggedUser.id,
+      user1: loggedInUser.id,
       user2: selectedUser.id,
-      content: `${loggedUser.username} viewed your profile.`,
+      content: `${loggedInUser.username} viewed your profile.`,
       type: 1,
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  //const photos = usersService.getSelectedUserPhotos(selectedUser.id)
-  //console.log("Photos of selecetd user", photos)
+  const dispatch = useDispatch()
+  const likeButtonRef = useRef()
+  const handleLike = (userToLike) => {
+    dispatch(likeUser(loggedInUser, userToLike))
+    likeButtonRef.current.style.visibility = 'hidden'
+  }
 
   return (
-    <div className='flex flex-col ml-32 mr-56 mt-20 text-almost-white'>
-      <div className='flex flex-col items-center justify-center'>
+    <div className="flex flex-col ml-32 mr-56 mt-20 text-almost-white">
+      <div className="flex flex-col items-center justify-center">
         <img
           src={`http://localhost:3001/uploads/${selectedUser.profilePicture}`}
-          className='object-cover rounded-full h-60 w-60 border border-almost-white mb-10'
-          alt='profile-pic'
+          className="object-cover rounded-full h-60 w-60 border border-almost-white mb-10"
+          alt="profile-pic"
         ></img>
       </div>
-      <div className='flex justify-center mb-3'>
-        <p className='text-chitty-chitty text-3xl'>{selectedUser.username}</p>
+      <div className="flex justify-center mb-3">
+        <p className="text-chitty-chitty text-3xl">{selectedUser.username}</p>
       </div>
-			<div className='flex justify-center mb-3'>
-     		{showOnlineStatus()}
-      </div>
-      <div className='flex justify-center text-xl mt-10'>
-        <div className='flex flex-col justify-center'>
-          <div className='mb-3 flex items-center'><div className='text-chitty-chitty flex w-80'>Name:</div> {selectedUser.firstname} {selectedUser.lastname}</div>
-          <div className='mb-3 flex items-center'><div className='text-chitty-chitty flex w-80'>Age:</div> {selectedUser.age}</div>
-          <div className='mb-3 flex items-center'><div className='text-chitty-chitty flex w-80'>Gender Identity:</div> {selectedUser.genderIdentity}</div>
-          <div className='mb-3 flex items-center'>
-					<div className='text-chitty-chitty flex w-80'>Interested in:</div>{' '}
+      <div className="flex justify-center mb-3">{showOnlineStatus()}</div>
+      <div className="flex justify-center text-xl mt-10">
+        <div className="flex flex-col justify-center">
+          <div className="mb-3 flex items-center">
+            <div className="text-chitty-chitty flex w-80">Name:</div>{' '}
+            {selectedUser.firstname} {selectedUser.lastname}
+          </div>
+          <div className="mb-3 flex items-center">
+            <div className="text-chitty-chitty flex w-80">Age:</div>{' '}
+            {selectedUser.age}
+          </div>
+          <div className="mb-3 flex items-center">
+            <div className="text-chitty-chitty flex w-80">Gender Identity:</div>{' '}
+            {selectedUser.genderIdentity}
+          </div>
+          <div className="mb-3 flex items-center">
+            <div className="text-chitty-chitty flex w-80">Interested in:</div>{' '}
             {selectedUser.gender_interest === 'female' ? 'women' : 'men'}
           </div>
-          <div className='mb-3 flex items-center'><div className='text-chitty-chitty flex w-80'>City:</div> {selectedUser.city}</div>
-          <div className='mb-3 flex items-center'><div className='text-chitty-chitty flex w-80'>Country:</div> {selectedUser.country}</div>
-          <div className='mb-3 flex items-center'><div className='text-chitty-chitty flex w-80'>Fame:</div> {selectedUser.fame}</div>
+          <div className="mb-3 flex items-center">
+            <div className="text-chitty-chitty flex w-80">City:</div>{' '}
+            {selectedUser.city}
+          </div>
+          <div className="mb-3 flex items-center">
+            <div className="text-chitty-chitty flex w-80">Country:</div>{' '}
+            {selectedUser.country}
+          </div>
+          <div className="mb-3 flex items-center">
+            <div className="text-chitty-chitty flex w-80">Fame:</div>{' '}
+            {selectedUser.fame}
+          </div>
         </div>
       </div>
-
-      <div className='flex flex-row justify-center items-center mt-10 '>
-        <div className='flex gap-96 '>
-          <button className='w-10 mt-4'>
-            {/* <button className="w-10 mt-4" onClick={setupChat}> */}
-            <HeartIcon className='h-12 w-12' />
-          </button>
-          <button className='w-10 mt-4'>
-            <XMarkIcon className='h-12 w-12' />
+      <div className="flex flex-row justify-center items-center mt-10 ">
+        <div className="flex gap-96 ">
+          {!hasLiked ? (
+            <button
+              ref={likeButtonRef}
+              className="w-10 mt-4"
+              onClick={() => handleLike(selectedUser)}
+            >
+              <HeartIcon className="h-12 w-12" />
+            </button>
+          ) : (
+            ''
+          )}
+          <button className="w-10 mt-4">
+            <XMarkIcon className="h-12 w-12" />
           </button>
         </div>
       </div>
