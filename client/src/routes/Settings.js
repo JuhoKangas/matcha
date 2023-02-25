66d6a19f51b35ddf1b48ba27854da9a860a92b5c
Juho Kangas <jkangas@student.hive.fi>
2023-02-25 21:02:12 +0200

    add length check for bio

--- client/src/routes/Settings.js
+++ client/src/routes/Settings.js
@@ -169,6 +169,10 @@ const Settings = ({ user }) => {
       errors.tags = 'Okay cowboy chill, select up to 5 tags'
     }
 
+    if (formData.bio.length > 255) {
+      errors.bio = 'The bio can be maximum of 255 characters'
+    }
+
     return errors
   }
 
@@ -240,14 +244,14 @@ const Settings = ({ user }) => {
       <div className='flex justify-center items-center'>
         <form
           onSubmit={handleUpdate}
-          className="bg-almost-black shadow-sm rounded px-10 pt-10 pb-8"
+          className='bg-almost-black shadow-sm rounded px-10 pt-10 pb-8'
         >
-          <div className="flex flex-row justify-center space-x-20">
-            <div className=" mb-15">
-              <div className="mb-4">
+          <div className='flex flex-row justify-center space-x-20'>
+            <div className=' mb-15'>
+              <div className='mb-4'>
                 <label
-                  className=" font-montserrat font-medium mb-2 text-almost-white"
-                  htmlFor="userName"
+                  className=' font-montserrat font-medium mb-2 text-almost-white'
+                  htmlFor='userName'
                 >
                   Username
                 </label>
@@ -272,8 +276,8 @@ const Settings = ({ user }) => {
               <div>
                 <div className='mb-4'>
                   <label
-                    className=" font-montserrat font-medium mb-2 text-almost-white"
-                    htmlFor="email"
+                    className=' font-montserrat font-medium mb-2 text-almost-white'
+                    htmlFor='email'
                   >
                     Email
                   </label>
@@ -298,8 +302,8 @@ const Settings = ({ user }) => {
 
               <div className='mb-4'>
                 <label
-                  className=" font-montserrat font-medium mb-2 text-almost-white"
-                  htmlFor="password"
+                  className=' font-montserrat font-medium mb-2 text-almost-white'
+                  htmlFor='password'
                 >
                   Password
                 </label>
@@ -322,8 +326,8 @@ const Settings = ({ user }) => {
 
               <div className='mb-4'>
                 <label
-                  className=" font-montserrat font-medium mb-2 text-almost-white"
-                  htmlFor="password"
+                  className=' font-montserrat font-medium mb-2 text-almost-white'
+                  htmlFor='password'
                 >
                   Repeat Password
                 </label>
@@ -348,8 +352,8 @@ const Settings = ({ user }) => {
 
               <div className='mb-4'>
                 <label
-                  className=" font-montserrat font-medium mb-2 text-almost-white"
-                  htmlFor="firstname"
+                  className=' font-montserrat font-medium mb-2 text-almost-white'
+                  htmlFor='firstname'
                 >
                   First Name
                 </label>
@@ -373,8 +377,8 @@ const Settings = ({ user }) => {
 
               <div className='mb-4'>
                 <label
-                  className=" font-montserrat font-medium mb-2 text-almost-white"
-                  htmlFor="lastname"
+                  className=' font-montserrat font-medium mb-2 text-almost-white'
+                  htmlFor='lastname'
                 >
                   Last Name
                 </label>
@@ -397,11 +401,11 @@ const Settings = ({ user }) => {
               </div>
             </div>
 
-            <div className=" mb-20">
-              <div className="mb-4">
+            <div className=' mb-20'>
+              <div className='mb-4'>
                 <label
-                  htmlFor="age"
-                  className=" font-montserrat font-medium mb-2 text-almost-white"
+                  htmlFor='age'
+                  className=' font-montserrat font-medium mb-2 text-almost-white'
                 >
                   Age
                 </label>
@@ -425,8 +429,8 @@ const Settings = ({ user }) => {
 
               <div className=''>
                 <label
-                  htmlFor="gender-identity"
-                  className=" font-montserrat font-medium mb-4 text-almost-white"
+                  htmlFor='gender-identity'
+                  className=' font-montserrat font-medium mb-4 text-almost-white'
                 >
                   Gender
                   <div className='flex justify-start mb-10'>
@@ -478,8 +482,8 @@ const Settings = ({ user }) => {
 
               <div className='mb-4'>
                 <label
-                  className=" font-montserrat font-medium mb-2 text-almost-white"
-                  htmlFor="city"
+                  className=' font-montserrat font-medium mb-2 text-almost-white'
+                  htmlFor='city'
                 >
                   City
                 </label>
@@ -502,8 +506,8 @@ const Settings = ({ user }) => {
 
               <div className='mb-4'>
                 <label
-                  className=" font-montserrat font-medium mb-2 text-almost-white"
-                  htmlFor="country"
+                  className=' font-montserrat font-medium mb-2 text-almost-white'
+                  htmlFor='country'
                 >
                   Country
                 </label>
@@ -526,8 +530,8 @@ const Settings = ({ user }) => {
 
               <div className=''>
                 <label
-                  htmlFor="gender-interest"
-                  className=" font-montserrat font-medium mb-2 text-almost-white"
+                  htmlFor='gender-interest'
+                  className=' font-montserrat font-medium mb-2 text-almost-white'
                 >
                   Show Me
                 </label>
@@ -578,17 +582,17 @@ const Settings = ({ user }) => {
               </div>
             </div>
           </div>
-          <div className="flex flex-col justify-center ml-96 mr-96">
+          <div className='flex flex-col justify-center ml-96 mr-96'>
             <label
-              className="font-montserrat font-medium mb-2 text-almost-white"
-              htmlFor="bio"
+              className='font-montserrat font-medium mb-2 text-almost-white'
+              htmlFor='bio'
             >
               Bio
             </label>
             <textarea
-              id="bio"
-              name="bio"
-              className="mt-1
+              id='bio'
+              name='bio'
+              className='mt-1
 								mb-10
 								font-montserrat
 								w-full
@@ -597,29 +601,29 @@ const Settings = ({ user }) => {
 								shadow-sm
 								text-gray-700
 								focus:border-chitty-chitty focus:ring focus:ring-chitty-chitty focus:ring-opacity-20
-								resize-y"
+								resize-y'
               {...initialBio}
               required
             ></textarea>
           </div>
-          <div className="flex flex-col justify-center ml-96 mr-96">
-            <label className="flex flex-col font-montserrat font-medium text-almost-white mb-3">
+          <div className='flex flex-col justify-center ml-96 mr-96'>
+            <label className='flex flex-col font-montserrat font-medium text-almost-white mb-3'>
               Interests
             </label>
-            <div className="flex flex-wrap justify-center font-montserrat font-medium mb-2 text-almost-white text-lg">
+            <div className='flex flex-wrap justify-center font-montserrat font-medium mb-2 text-almost-white text-lg'>
               {newTags.map((tag) => (
                 <Tag key={tag.id} tagName={tag.tagname} onClick={handleTag} />
               ))}
             </div>
           </div>
-          <div className="flex justify-start ml-96 mt-10">
+          <div className='flex justify-start ml-96 mt-10'>
             <label
-              className="flex justify-center font-montserrat font-medium text-almost-white"
-              htmlFor="lastname"
+              className='flex justify-center font-montserrat font-medium text-almost-white'
+              htmlFor='lastname'
             >
               Profile Picture
             </label>
-            <div className="flex flex-col items-center justify-center gap-12 md:mb-0 mb-10 mt-10">
+            <div className='flex flex-col items-center justify-center gap-12 md:mb-0 mb-10 mt-10'>
               {file ? (
                 <img
                   className='object-cover rounded-full h-60 w-60'
